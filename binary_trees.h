5fe71e894520a2384125ae43bee1771150f10613
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-03-01 15:39:42 -0800

    Iitial commits: README, Header file

---
+++ binary_trees.h
@@ -0,0 +1,21 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stdlib.h> /* mainly for malloc and free */
+
+/**
+ * struct binary_tree_s - struct defining a binary tree node
+ * @n: data for the node as integer
+ * @parent: of node 
+ * @left: left child of node/subtree
+ * @right: right child of node/subtree
+ */
+
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+}
+#endif
