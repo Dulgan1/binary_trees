8450f5f305524dfaa14eaaf998ddf9309bba154d
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-03-02 08:03:35 -0800

    Updated task 2

--- binary_trees.h
+++ binary_trees.h
@@ -35,4 +35,31 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
  */
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+/**
+ * binary_tree_print - Prints a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to print
+ */
+void binary_tree_print(const binary_tree_t *tree);
+
+/**
+ * _height - Measures the height of a binary tree
+ *                                                                                       * @tree: Pointer to the node to measures the height                                     *
+ * Return: The height of the tree starting at @node
+ */
+
+size_t _height(const binary_tree_t *tree);
+
+/**
+ * print_t - Stores recursively each level in an array of strings
+ *
+ * @tree: Pointer to the node to print
+ * @offset: Offset to print
+ * @depth: Depth of the node
+ * @s: Buffer
+ *
+ * Return: length of printed tree after process
+ */
+int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
 #endif
