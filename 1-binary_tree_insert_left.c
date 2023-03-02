19ad8f82c45b431708d41bfe63385e1fc247f5e9
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-03-02 08:40:02 -0800

    Initial commit for task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts new node by left of parent
+ * @parent: parent to insert node to by left
+ * @value: value for data of node
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	if (!parent)
+		return (NULL);
+	node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (!node)
+		return (NULL);
+
+	node->n = value;
+	node->parent = parent;
+	node->right = NULL;
+
+	if (parent->left != NULL)
+	{
+		node->left = parent->left;
+		parent->left->parent = node;
+		parent->left = node;
+	}
+	else 
+		parent->left = node;
+
+	return (NULL);
+}
