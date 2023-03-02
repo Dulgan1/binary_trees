638b49a683b25c82cf621d6171675bd1770a52b2
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-03-01 16:00:32 -0800

    Declared type for 2-ary tree

--- binary_trees.h
+++ binary_trees.h
@@ -11,11 +11,12 @@
  * @right: right child of node/subtree
  */
 
-struct binary_tree_s
+typedef struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-}
+} binary_tree_t;
+
 #endif
