8ad89bec23e4fd8a974cc7df100d13fd241d491e
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-03-03 06:05:42 -0800

    Initial commit for task 5

--- binary_trees.h
+++ binary_trees.h
@@ -60,4 +60,12 @@ void binary_tree_delete(binary_tree_t *tree);
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node);
+
+/**
+ * binary_tree_is_root - checks if a node is a root
+ * @node: node to check
+ * Return: returns 1 if truee, 0 if not or node is NULL
+ */
+
+int binary_tree_is_root(const binary_tree_t *node);
 #endif /* BINARY_TREES_H */
