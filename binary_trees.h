af8fac525e981690cfba342985cb07051da34052
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-03-04 05:08:52 -0800

    Initial commit for task 7

--- binary_trees.h
+++ binary_trees.h
@@ -76,4 +76,12 @@ int binary_tree_is_root(const binary_tree_t *node);
  */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
+/**
+ * binary_tree_inorder - goes through a binary tree inorder trasversal
+ * @tree: tree to go through
+ * @func: function to work on each node
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 #endif /* BINARY_TREES_H */
